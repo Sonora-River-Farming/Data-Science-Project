ac1616377a5cf2a8e753f23c15c5bcb2875590dc
ArianMG <arianmilanes@gmail.com>
2024-10-04 18:12:00 -0700

    feat: update dataset

--- modules/dataset.py
+++ modules/dataset.py
@@ -1,29 +1,44 @@
 from pathlib import Path
-
 import typer
 from loguru import logger
 from tqdm import tqdm
-
-from modules.config import PROCESSED_DATA_DIR, RAW_DATA_DIR
+from dataset_modules.downloader import download_file
+from dataset_modules.processor import process_data
+from dvc_modules.dvc_manager import check_dvc_repo, add_dvc_remote
+from modules.config import PROCESSED_DATA_DIR, RAW_DATA_DIR, URL_LIST
 
 app = typer.Typer()
 
-
 @app.command()
 def main(
-    # ---- REPLACE DEFAULT PATHS AS APPROPRIATE ----
-    input_path: Path = RAW_DATA_DIR / "dataset.csv",
-    output_path: Path = PROCESSED_DATA_DIR / "dataset.csv",
-    # ----------------------------------------------
+    #url: str = typer.Option(None, help="URL of the dataset to download"),
+    #file: str = typer.Option(None, help="File name to save the dataset (optional)"),
+    #input_path: Path = typer.Option(None, help="Optional path to save the dataset"),
+    # output_path: Path = PROCESSED_DATA_DIR / "dataset.csv",
 ):
-    # ---- REPLACE THIS WITH YOUR OWN CODE ----
-    logger.info("Processing dataset...")
-    for i in tqdm(range(10), total=10):
-        if i == 5:
-            logger.info("Something happened for iteration 5.")
-    logger.success("Processing dataset complete.")
-    # -----------------------------------------
+    # if input_path is None:
+    #     if file is None:
+    #         typer.echo("You must provide either a file name or an input path.")
+    #         raise typer.Exit()
+    #     input_path = RAW_DATA_DIR / file
 
+    # Verificar si estamos en un repositorio DVC
+    check_dvc_repo()
+
+    # Check remote
+    add_dvc_remote()   
+    
+    # Download files
+    if URL_LIST:
+        for url in URL_LIST:
+            input_path = RAW_DATA_DIR / url['file']
+            download_file(url=url['url'], info=url['info'], input_path=input_path)
+        logger.success(f"All files have been downloaded successfully !")
+    else:
+        logger.error(f"URL_LIST is empty")
+    
+    # Process data
+    process_data()
 
 if __name__ == "__main__":
     app()
